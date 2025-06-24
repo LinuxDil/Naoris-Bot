40596c26a44b9da2cebb2bc7ce084788fdfc47a8
Jack Sparrow <133944655+LinuxDil@users.noreply.github.com>
2025-06-24 13:00:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ class NaorisProtocol:
         )
 
     def welcome(self):
-        figlet = Figlet(font='ansi shadow')  # Ganti font di sini
+        figlet = Figlet(font='block')  # Ganti font di sini
         banner = figlet.renderText("Naoris Bot")
 
         terminal_width = shutil.get_terminal_size().columns
